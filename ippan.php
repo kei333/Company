7de8e2c75e0870c3f441b7220da887e2598d7d02
kei333 <46550291+kei333@users.noreply.github.com>
2019-03-07 13:31:41 +0900

    Add files via upload

---
+++ ippan.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if(isset($_POST['logout'])){
+    $_SESSION = array();
+    @session_destroy();
+    header('Location: http://localhost/forever/login.php');
+    exit;
+}
+?>
+<html> 
+    <head> 
+            <meta charset="UTF-8"> 
+            <title>一般ユーザ</title> 
+    </head> 
+    <body> 
+        <h1>ホーム画面</h1> 
+        <?php
+        print $_SESSION['name'];
+        ?>
+        <form action="ippan.php" method="POST">
+        <p>一般ユーザログイン完了</p> 
+        <input type="submit" id="logout" name="logout" value="ログアウト">
+        </form>
+    </body>
+</html> 
---
+++ kanrisya.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if(isset($_POST['logout'])){
+    $_SESSION = array();// セッションの変数のクリア
+    @session_destroy();// セッションクリア
+    header('Location: http://localhost/forever/login.php');
+    exit;
+}
+?>
+<html> 
+    <head> 
+            <meta charset="UTF-8"> 
+            <title>管理者</title> 
+    </head> 
+    <body> 
+        <h1>ホーム画面</h1> 
+        <?php
+        print $_SESSION['name'];
+        ?>
+        <form action="kanrisya.php" method="POST">
+        <p>管理者ログイン完了</p> 
+        <input type="submit" id="logout" name="logout" value="ログアウト">
+        </form>
+    </body> 
+</html> 
