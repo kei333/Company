d9ba38a544e04ff3891ca1ef4986c2a75d33e3ff
kei333 <46550291+kei333@users.noreply.github.com>
2019-03-15 08:45:35 +0900

    Add files via upload

--- login.php
+++ login.php
@@ -17,12 +17,11 @@ if(isset($_POST['login'])){
             foreach ($pdo->query('SELECT * FROM user WHERE id="KANRISYA"') as $row2){
                 var_dump($row2);
             }*/
-            
             $stmt = $pdo->prepare('SELECT * FROM user WHERE id = "?"');
             $stmt->bindValue(1, $id, PDO::PARAM_STR);//疑問符プレースホルダを使用して1からパラメータを始める
-            
+            var_dump($stmt);
             //オブジェクトを配列として使用できない対策
-            //$_SESSION['name']= $pdo->prepare('SELECT name FROM user WHERE id=?');//sessonにidのname格納
+            $_SESSION['name']= $pdo->prepare('SELECT name FROM user WHERE id=?');//sessonにidのname格納
             $pass = $pdo->prepare('SELECT password FROM user WHERE id=?');//パスワード格納
             $flg = $pdo->prepare('SELECT admin_flg FROM user WHERE id=?');//権限格納
             //パスワードが一致したとき
